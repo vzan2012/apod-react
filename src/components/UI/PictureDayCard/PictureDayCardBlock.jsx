df8d7b821fc06d1caef24c0741721340f3c254d6
Deepak Guptha <vzan2012@gmail.com>
2024-04-25 19:12:13 -0400

    fix: update media display

--- src/components/UI/PictureDayCard/PictureDayCardBlock.jsx
+++ src/components/UI/PictureDayCard/PictureDayCardBlock.jsx
@@ -2,7 +2,29 @@ import { block } from "million";
 import { Row, Col, Container } from "react-bootstrap";
 
 const PictureDayCard = ({ dataResponse }) => {
-  const { title, date, url, explanation } = dataResponse;
+  const { title, date, url, media_type, explanation } = dataResponse;
+
+  /**
+   * Check the data media type is image or video
+   *
+   * @param {*} media_type
+   * @returns {*}
+   */
+  const checkMediaType = (media_type) => {
+    if (media_type === "image") {
+      return <img src={url} title={title} className="img-fluid d-img" />;
+    } else {
+      return (
+        <iframe
+          className="ytvideo"
+          src={url}
+          title={title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      );
+    }
+  };
+
   return (
     <Container>
       <Row>
@@ -16,7 +38,7 @@ const PictureDayCard = ({ dataResponse }) => {
           </span>
         </Col>
         <Col xs={12} className="media-section mb-3">
-          <img src={url} title={title} className="img-fluid d-img" />
+          {checkMediaType(media_type)}
         </Col>
         <Col xs={12} className="explanation mb-3">
           <p>{explanation}</p>
